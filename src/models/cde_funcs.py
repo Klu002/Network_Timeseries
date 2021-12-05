09055cfe62e0fa36bd308b6a48979476de8f5f66
madhavramesh <madhavramesh02@gmail.com>
2021-12-05 18:18:22 -0500

    Implemented ODE class

--- src/models/cde_funcs.py
+++ src/models/cde_funcs.py
@@ -1,3 +1,15 @@
 import math 
 import torch 
+from torch import nn
 import torchcde
+
+class CDEFunc(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(CDEFunc, self).__init__()
+        self.lin1 = nn.Linear(input_dim, hidden_dim)
+        self.lin2 = nn.Linear(hidden_dim, hidden_dim)
+        self.lin3 = nn.Linear(hidden_dim, hidden_dim * output_dim)
+        self.relu = nn.RELU(inplace=True)
+
+    def forward(self, x, t):
+
