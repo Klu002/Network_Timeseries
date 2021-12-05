b21582f9c05ae572eacefabbf14602fae5d370b2
Kevin Lu <kevin_lu2@brown.edu>
2021-12-05 18:12:15 -0500

    finished test function

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import math
 import torch
 import torchcde
 
-def train(num_epochs: int, data, optimizer, interpolation, loss_func):
+def train(model, num_epochs: int, data, optimizer, interpolation, loss_func):
   train_X, train_Y = data
   train_coeffs = interpolation(train_X)
 
-  train_dataset = torch.utils.data.TensorDataset(train_coeffs, train_y)
+  train_dataset = torch.utils.data.TensorDataset(train_coeffs, train_Y)
   train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=32)
 
   for epoch in range(num_epochs):
@@ -18,6 +18,14 @@ def train(num_epochs: int, data, optimizer, interpolation, loss_func):
           optimizer.step()
           optimizer.zero_grad()
       print('Epoch: {}   Training loss: {}'.format(epoch, loss.item()))
+
+def test(model, test_data, interpolation):
+  test_X, test_Y = test_data
+  pred = model(test_X).squeeze(-1)
+  prediction_matches = (pred == test_Y).to(test_Y.dtype)
+  proportion_correct = prediction_matches.sum() / test_Y.size(0)
+
+  return proportion_correct
 
 def main():
   #Hyperparameters
